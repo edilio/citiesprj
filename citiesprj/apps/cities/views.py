1d9415620ef45447e0e0cf685645257180b458a4
Edilio Gallardo <edilio73@gmail.com>
2016-04-25 07:35:01 -0400

    second blog

--- citiesprj/apps/cities/views.py
+++ citiesprj/apps/cities/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-
 from rest_framework import filters, viewsets
 
 from .serializers import CitySerializer
@@ -11,4 +8,8 @@ class CityViewSet(viewsets.ModelViewSet):
     model = City
     serializer_class = CitySerializer
     queryset = City.objects.all()
+    filter_backends = (filters.SearchFilter, filters.OrderingFilter, filters.DjangoFilterBackend)
+    search_fields = ('zip_code', 'city')
+    ordering_fields = ('zip_code', 'city')
+    filter_fields = ('state',)
 
